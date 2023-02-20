04f4b174681693120d9f726682d847f1cb50b83f
Zion Miller <djzionmiller@gmail.com>
2023-02-19 20:47:55 -0800

    initial rough idea for app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { WebView } from 'react-native-webview';
+import { Linking, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Define an array of apps to be blocked
+const BLOCKED_APPS = ['com.facebook.ios', 'com.twitter.android', 'com.instagram.android'];
+
+class App extends Component {
+  state = {
+    showPaywall: false,
+    currentApp: null,
+  };
+
+  componentDidMount() {
+    // Request permission to access installed apps
+    Linking.getInitialURL()
+      .then(url => {
+        if (url) {
+          // Get the package name from the URL
+          const packageName = url.replace('package:', '');
+          // Check if the package name is in the list of blocked apps
+          if (BLOCKED_APPS.includes(packageName)) {
+            this.setState({ showPaywall: true });
+          }
+          this.setState({ currentApp: packageName });
+        }
+      })
+      .catch(err => console.error('Error getting initial URL', err));
+  }
+
+  render() {
+    return (
+      <View>
+        {this.state.showPaywall ? (
+          <WebView source={{ uri: 'https://stripe.com' }} />
+        ) : null}
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>PP PooPoo Check!!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#808080',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
